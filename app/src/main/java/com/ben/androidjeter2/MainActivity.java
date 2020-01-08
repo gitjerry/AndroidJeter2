7446f6552b7d107c84bfb21a8988d44f72cfe9eb
libenqi <libenqi1@le.com>
2020-01-08 15:42:14 +0800

    init2

--- app/src/main/java/com/ben/androidjeter2/MainActivity.java
+++ app/src/main/java/com/ben/androidjeter2/MainActivity.java
@@ -3,6 +3,7 @@ package com.ben.androidjeter2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//https://github.com/gitjerry/AndroidJeter2.git
 public class MainActivity extends AppCompatActivity {
 
     @Override
